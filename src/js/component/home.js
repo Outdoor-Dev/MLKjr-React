2f06e4c9b8574d1897a86110fb298d8fc838571d
Outdoor-Dev <saltairpix@gmail.com>
2021-01-12 14:14:14 +0000

    initial

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import MLK from "../../img/MLK.png";
+import peace from "../../img/peace.png";
+import line from "../../img/line.png";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="mlk">
+				<img src={MLK} />
+			</div>
+			<div className="bubble">
+				<img src="https://www.clker.com/cliparts/U/8/k/x/W/9/cartoon-speech-bubble-md.png" />
+			</div>
+
+			<img className="peace" src={peace} />
+			<img className="line" src={line} />
+		</>
 	);
 }
