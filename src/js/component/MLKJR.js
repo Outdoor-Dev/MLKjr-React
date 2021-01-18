b463664e633f05c591594ee8375e0178a296225c
Outdoor-Dev <saltairpix@gmail.com>
2021-01-18 09:04:38 +0000

    update

--- src/js/component/MLKJR.js
+++ src/js/component/MLKJR.js
@@ -10,7 +10,7 @@ export const MLKJR = () => {
 				autoPlay
 				loop
 				type="video/mp4"
-				src="https://v.ftcdn.net/03/88/40/10/700_F_388401044_mXzGhaniJub0yQUU1stRZIwXUub5MTbz_ST.mp4">
+				src="https://v.ftcdn.net/02/18/55/89/700_F_218558977_UainYTjWpK1gpRZWqLLKC5pzOYQkmNP7_ST.mp4">
 				Your browser does not support the video tag{" "}
 			</video>
 			<div className="mlk">
